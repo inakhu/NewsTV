3270d1d629acb471e868eace07c9a5c17b7a92ba
inakhu <hezecom2@gmail.com>
2021-01-04 14:03:36 +0100

    upgrade

--- www/app/services/HTSconstants.js
+++
@@ -1,7 +0,0 @@
-angular.module('htsApp.constants', [])
-    .constant('APP_SERVER',{
-        url:'http://nftvapp.com/',
-        lurl:'http://localhost/HEZECOM/frontier/news/',
-        wurl:'http://nftvapp.com/',
-        apikey:'HTSNFAPP'
-    });
